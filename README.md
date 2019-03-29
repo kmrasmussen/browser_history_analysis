bfe95e0659eab16a0b6b03a70dc5f5c2a99ec554
obscur <obscur@tuta.io>
2019-03-29 09:41:08 +0100

    .

---
+++ README.md
@@ -0,0 +1,20 @@
+# browser_history_analysis
+
+In Firefox the browsing history is stored in places.sqlite. Some documentation exists here:
+https://developer.mozilla.org/en-US/docs/Mozilla/Tech/Places/Database
+
+The browsing history seems to be a valuable data set for analysis of human behavior patterns
+on the internet. For example it may show the evolution of thoughts and interests in search
+behavior, addiction to social media, variability in the amount of time spent on each site,
+prediction of browsing. The browsing data is easily collected and generates thousands of
+data points a month. Considering the difficulty in predicting the ways in which personal
+datasets can be exploited in the future, it seems rational to backup these browsing history
+databases.
+
+At the current moment analysis.py includes only a very simple analysis. It reads the
+moz_historyvisits table which contains the full browsing history. Each row contains an ID
+to a place corresponds to the moz_places table. By going through historyvists and finding the
+URL and getting simply the hostname, it is possible to identify which sites have been visited
+the most and the distribution.
+
+![hostnameVisitsDist](./images/hostnameVisitsDist.jpg)
---
+++ analysis.py
@@ -0,0 +1,56 @@
+import sqlite3
+from urllib.parse import urlparse
+import numpy as np
+import matplotlib.pyplot as plt
+
+databaseFilename = 'places.sqlite'
+conn = sqlite3.connect(databaseFilename)
+c = conn.cursor()
+
+c.execute('SELECT * FROM moz_historyvisits')
+historyvisits = c.fetchall()
+
+c.execute('SELECT * FROM moz_places')
+places = c.fetchall()
+
+hostNames = {}
+
+print(len(places))
+
+for visit in historyvisits:
+    #print('\nNow:')
+    # place number of visit
+    visitPlaceId = visit[2]
+    if(visitPlaceId < 1):
+        print('Less than 1!')
+    #print('VisitPlaceId:' + str(visitPlaceId))
+    # get url associated with that place, table is 1-indexed so we have to subtract 1
+    c.execute('SELECT * FROM moz_places WHERE id = ' + str(visitPlaceId))
+    place = c.fetchone()
+    visitUrl = place[1]
+    #print('Url: ' + str(visitUrl))
+    # get only hostname of url using urllib
+    visitHostname = urlparse(visitUrl).hostname
+    #print('Hostname:' + str(visitUrl))
+    if visitHostname in hostNames:
+        hostNames[visitHostname] = hostNames[visitHostname] + 1
+        #print(hostNames[visitHostname])
+    else:
+        hostNames[visitHostname] = 1
+        #print(hostNames[visitHostname])
+
+conn.close()
+
+visitList = []
+for key, value in hostNames.items():
+    visitList.append(value)
+
+total = np.sum(visitList)
+
+visitList = sorted(visitList, reverse=True)
+
+plt.scatter(np.arange(len(visitList)), visitList)
+plt.title('Distribution of visits to different hostnames')
+plt.ylabel('visits')
+plt.xlabel('sites')
+plt.show()
